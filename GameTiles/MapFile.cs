453d6693d93cc25e381a1d226f9e76460bf152d6
cal900 <cal900@hotmail.com>
2016-01-10 19:48:40 -0500

    commit untracked files

---
+++ GameTiles/MapFile.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GameTiles.Tiles;
+
+namespace GameTiles
+{
+    public struct MapFile
+    {
+        Tile[,] MapTiles;
+    }
+}
