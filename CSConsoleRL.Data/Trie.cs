94cc3382de9e772e993d13da1b83acc925ad60fa
cal900 <jsssheehy@gmail.com>
2018-12-28 01:05:10 -0500

    Progress on Trie

--- CSConsoleRL.Data/Trie.cs
+++ CSConsoleRL.Data/Trie.cs
@@ -6,26 +6,29 @@ using System.Threading.Tasks;
 
 namespace CSConsoleRL.Data
 {
+    /// <summary>
+    /// A space char is used to indicate that branch level is also an endpoint
+    /// </summary>
     public class Trie
     {
-        private Dictionary<char, TrieBranch> branches;
+        private Dictionary<char, TrieBranch> _branches;
 
         public Trie()
         {
-            branches = new Dictionary<char, TrieBranch>();
+            _branches = new Dictionary<char, TrieBranch>();
         }
 
         public void Insert(string remainingBranches)
         {
             if(remainingBranches.Length > 0)
             {
-                if(branches.ContainsKey(remainingBranches[0]))
+                if(_branches.ContainsKey(remainingBranches[0]))
                 {
-                    branches[remainingBranches[0]].Insert(remainingBranches);
+                    _branches[remainingBranches[0]].Insert(remainingBranches);
                 }
                 else
                 {
-                    branches.Add(remainingBranches[0], new TrieBranch(remainingBranches));
+                    _branches.Add(remainingBranches[0], new TrieBranch(remainingBranches));
                 }
             }
         }
@@ -39,7 +42,7 @@ namespace CSConsoleRL.Data
 
         private class TrieBranch
         {
-            private Dictionary<char, TrieBranch> branches;
+            private Dictionary<char, TrieBranch> _branches;
             public readonly char Value;
 
             public TrieBranch(string remainingBranches)
@@ -53,25 +56,58 @@ namespace CSConsoleRL.Data
             {
                 if (remainingBranches.Length > 0)
                 {
-                    if (branches.ContainsKey(remainingBranches[0]))
+                    if (_branches.ContainsKey(remainingBranches[0]))
                     {
-                        branches[remainingBranches[0]].Insert(remainingBranches.Substring(1));
+                        _branches[remainingBranches[0]].Insert(remainingBranches.Substring(1));
                     }
                     else
                     {
-                        branches.Add(remainingBranches[0], new TrieBranch(remainingBranches.Substring(1)));
+                        _branches.Add(remainingBranches[0], new TrieBranch(remainingBranches.Substring(1)));
                     }
                 }
             }
 
-            public List<string> GetPathBranches(string path, List<string> branches)
+            public void GetAllChildBranches(string localPath, List<string> paths)
             {
-                //If reached end of the road return
-                if(path.Length == 1)
+                if(_branches.Count == 0)
                 {
-                    branches.Add()
+                    paths.Add(localPath + Value);
+                }
+                else
+                {
+                    foreach (var branch in _branches)
+                    {
+                        GetAllChildBranches(localPath + Value, paths);
+                    }
+                }
+            }
+
+            public void GetPathBranches(string path, string fullPath, List<string> branches)
+            {
+                if(path.Length > 0)
+                {
+                    if(_branches.ContainsKey(path[0]))
+                    {
+                        //Go into child branch
+                        GetPathBranches(path.Substring(1), fullPath, branches);
+                    }
+                    else
+                    {
+                        //Reached end of road
+                        return;
+                    }
+                }
+                //If reached end of the road return all child branches
+                else
+                {
+                    foreach (KeyValuePair<char, TrieBranch> branch in _branches)
+                    {
+                        branches.Add(branch.)
+                    }
                 }
             }
         }
     }
 }
+
+//Navigate until hit end of string, then return all remaining branches
