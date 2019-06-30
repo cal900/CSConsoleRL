ccf1768696797ab947c7857450f56f215677e12f
Jason <jsssheehy@gmail.com>
2019-06-29 23:04:00 -0400

    More shell stuff

--- CSConsoleRL/GameSystems/ShellSystem.cs
+++ CSConsoleRL/GameSystems/ShellSystem.cs
@@ -3,12 +3,22 @@ using CSConsoleRL.Entities;
 using CSConsoleRL.Events;
 using CSConsoleRL.Game.Managers;
 using SFML.Window;
+using System;
 using System.Collections.Generic;
 
 namespace CSConsoleRL.GameSystems
 {
     public class ShellSystem : GameSystem
     {
+        struct ShellCommand
+        {
+            string Cmd;
+            string Desc;
+            Func<List<string>, string> ShellFunction;
+        }
+
+        private Dictionary<string, Func<List<string>, string>> _supportedShellFunctions;
+
         public ShellSystem(GameSystemManager manager)
         {
             SystemManager = manager;
@@ -27,6 +37,21 @@ namespace CSConsoleRL.GameSystems
         public override void HandleMessage(IGameEvent gameEvent)
         {
         
-        }        
+        }
+
+        /// <summary>
+        /// Define supported shell functions here for use through the game terminal
+        /// </summary>
+        private void DefineSupportedShellFunctions()
+        {
+            _supportedShellFunctions.Add("ce", CreateEntity);
+        }
+
+        private string CreateEntity(List<string> inputs)
+        {
+            return "";
+        }
+
+
     }
 }
