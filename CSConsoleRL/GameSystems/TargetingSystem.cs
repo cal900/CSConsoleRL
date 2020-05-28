9bb66786f754342b4174bc73b87dc6065d3cd431
J J <jsssheehy@gmail.com>
2020-05-28 01:02:12 -0400

    Added empty TargetingSystem for the future

---
+++ CSConsoleRL/GameSystems/TargetingSystem.cs
@@ -0,0 +1,35 @@
+using CSConsoleRL.Components;
+using CSConsoleRL.Entities;
+using CSConsoleRL.Events;
+using CSConsoleRL.Game.Managers;
+using SFML.Window;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSConsoleRL.GameSystems
+{
+    public class TargetingSystem : GameSystem
+    {
+        public TargetingSystem()
+        {
+
+        }
+
+        public override void InitializeSystem()
+        {
+
+        }
+
+        public override void HandleMessage(IGameEvent gameEvent)
+        {
+
+        }
+
+        public override void AddEntity(Entity entity)
+        {
+            
+        }
+    }
+}
