f7f91a2a0abd19a74d70cb64d8e0fa6fd952ac35
J J <jsssheehy@gmail.com>
2020-09-26 03:36:54 -0400

    Added file

--- CSConsoleRL/Helpers/UiRenderHelper.cs
+++ CSConsoleRL/Helpers/UiRenderHelper.cs
@@ -1,10 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
+using SFML.System;
+using GameTiles.Tiles;
+using CSConsoleRL.Logging;
 
 namespace CSConsoleRL.Helpers
 {
-  class UiRenderHelper
+  public sealed class UiRenderHelper
   {
+    private static UiRenderHelper _instance;
+    public static UiRenderHelper Instance
+    {
+      get
+      {
+        if (_instance == null)
+        {
+          _instance = new UiRenderHelper();
+        }
+        return _instance;
+      }
+    }
+
+    private UiRenderHelper()
+    {
+
+    }
+
+
   }
 }
