c34b9a2d5bba90b6fe4abdc2f14870c98be4a67c
J J <jsssheehy@gmail.com>
2021-05-01 01:37:11 -0400

    Added combat system but no logic

--- CSConsoleRL/GameSystems/CombatSystem.cs
+++ CSConsoleRL/GameSystems/CombatSystem.cs
@@ -9,11 +9,11 @@ using CSConsoleRL.Helpers;
 
 namespace CSConsoleRL.GameSystems
 {
-  public class CombatSystem : GameSystem
+  public class HealthSystem : GameSystem
   {
     private readonly GameStateHelper _gameStateHelper;
 
-    public CombatSystem(GameSystemManager manager, GameStateHelper gameStateHelper)
+    public HealthSystem(GameSystemManager manager, GameStateHelper gameStateHelper)
     {
       SystemManager = manager;
       _systemEntities = new List<Entity>();
@@ -27,10 +27,7 @@ namespace CSConsoleRL.GameSystems
 
     public override void AddEntity(Entity entity)
     {
-      if (entity.Components.ContainsKey(typeof(HealthComponent)))
-      {
-        _systemEntities.Add(entity);
-      }
+
     }
 
     public override void HandleMessage(IGameEvent gameEvent)
@@ -41,33 +38,8 @@ namespace CSConsoleRL.GameSystems
           var guid = (Guid)gameEvent.EventParams[0];
           var ent = _systemEntities.Where(e => e.Id == guid).FirstOrDefault();
           var amount = (int)gameEvent.EventParams[1];
-          ChangeEntityHealth(ent, amount);
           break;
       }
-    }
-
-    private void ChangeEntityHealth(Entity entity, int amount)
-    {
-      var healthComponent = entity.GetComponent<HealthComponent>();
-      var currentHealth = healthComponent.CurrentHealth;
-      var newHealth = currentHealth;
-
-      if (currentHealth + amount > healthComponent.MaxHealth)
-      {
-        newHealth = healthComponent.MaxHealth;
-      }
-      else if (currentHealth + amount < 0)
-      {
-        newHealth = 0;
-      }
-
-      // If health is 0, this entity is dead and needs to be removed
-      if (newHealth == 0)
-      {
-        SystemManager.RemoveEntity(entity);
-      }
-
-      healthComponent.CurrentHealth = newHealth;
     }
   }
 }
